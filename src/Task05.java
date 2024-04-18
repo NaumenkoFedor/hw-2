a7ec2f33983527949ce9b383a0c490de68acd5b8
FeN <feudorn@mail.ru>
2024-04-18 20:54:11 +0400

    add Task05.java.java

---
+++ src/Task05.java
@@ -0,0 +1,19 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+public class Task05 {
+    public static final int RADIUS = 3;
+
+    public Task05() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Задание 5");
+        double area = 28.274333882308138;
+        System.out.println("Площадь круга: " + Math.round(area));
+        double circusference = 18.84955592153876;
+        System.out.println("Длина окружности " + Math.round(circusference));
+    }
+}
