90ddb94cb006d34bfbe9c82834a00e9d17462bb8
FeN <feudorn@mail.ru>
2024-04-18 21:43:47 +0400

    add Task10.java.java

---
+++ src/Task11.java
@@ -0,0 +1,17 @@
+public class Task11
+{
+    public static void main(String[] args) {
+        System.out.println("задание 11");
+        long startSecond = 2_432_452L;
+        long days = startSecond / 86400;
+        long hours = (days) / 3600;
+        long minutes = (hours) / 60;
+        long seconds = (minutes) % 60;
+        System.out.println(days
+                + " "
+                + hours + " "
+                +
+                minutes + " "
+                + seconds);
+    }
+}
