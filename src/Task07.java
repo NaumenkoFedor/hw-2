4b0b0e052454271ba57344e689ddabe8e5e3e905
FeN <feudorn@mail.ru>
2024-04-18 21:01:16 +0400

    add Task07.java.java

---
+++ src/Task07.java
@@ -0,0 +1,11 @@
+public class Task07 {
+    public static final int YEAR_PERSENT = 10;
+    public static void main(String[] args) {
+        System.out.println("задание 7");
+        int money = 1000;
+        int itg = money + ((money * YEAR_PERSENT) * 2) / 100;
+        System.out.println("итоговая сумма "+ itg);
+
+
+    }
+}
