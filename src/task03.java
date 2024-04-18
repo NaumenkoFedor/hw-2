c40c162c80d4ec64f643caca652342164f1e1daa
FeN <feudorn@mail.ru>
2024-04-18 19:56:52 +0400

    add task02.java

---
+++ src/task03.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class task03 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("введите вещественное число с ненулевой дробной частью: ");
+        float n = Math.round(sc.nextFloat());
+        System.out.println("Result=" + n);
+
+    }
+}
