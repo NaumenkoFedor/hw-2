b9cc5fa9fc65dccb7605a990be3439cf81044652
FeN <feudorn@mail.ru>
2024-04-18 20:40:43 +0400

    add Task04.java.java

---
+++ src/Task05.java
@@ -0,0 +1,15 @@
+public class Task05 {
+    public static final int RADIUS = 3;
+    public static void main(String[] args){
+        System.out.println("Задание 5");
+        double area = Math.PI * (RADIUS * RADIUS);
+        System.out.println("Площадь круга: " + Math.round(area));
+        double circusference = Math.PI *2 *RADIUS;
+        System.out.println("Длина окружности: " + Math.round(circusference));
+
+
+
+
+
+    }
+}
