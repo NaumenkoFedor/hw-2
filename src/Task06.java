32044f2d6f4a8ae7ca55badf718c5b482f322ffd
FeN <feudorn@mail.ru>
2024-04-18 20:47:40 +0400

    add Task06.java.java

---
+++ src/Task06.java
@@ -0,0 +1,12 @@
+public class Task06 {
+    public static void main(String[] args) {
+        final int W = 5;
+        final int H = 10;
+        System.out.println("задание 6");
+        int s = H * W;
+        int p = (W +H) * 2;
+        System.out.println("площадь " + s);
+        System.out.println("периметр + p");
+
+    }
+}
